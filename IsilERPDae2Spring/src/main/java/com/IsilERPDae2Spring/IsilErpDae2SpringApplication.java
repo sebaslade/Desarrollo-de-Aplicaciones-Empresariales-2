d64695f4a57cbb061b0554d559f4aa919675a787
sebaslade <sebaslade29@gmail.com>
2025-05-19 15:27:49 -0500

    Proyecto con Spring

---
+++ IsilERPDae2Spring/src/main/java/com/IsilERPDae2Spring/IsilErpDae2SpringApplication.java
@@ -0,0 +1,13 @@
+package com.IsilERPDae2Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsilErpDae2SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsilErpDae2SpringApplication.class, args);
+	}
+
+}
---
+++ IsilERPDae2Spring/src/test/java/com/IsilERPDae2Spring/IsilErpDae2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IsilERPDae2Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsilErpDae2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
